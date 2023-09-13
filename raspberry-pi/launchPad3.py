c18c1a8f7d099b8e4062ebb13eb83a1209791244
jhelmke45 <jackhelmke45@gmail.com>
2023-09-13 10:36:23 -0400

    commented code!

--- raspberry-pi/launchPad3.py
+++ raspberry-pi/launchPad3.py
@@ -5,30 +5,30 @@ import time
 led1 = digitalio.DigitalInOut(board.GP1)
 led1.direction = digitalio.Direction.OUTPUT
 led2 = digitalio.DigitalInOut(board.GP2)
-led2.direction = digitalio.Direction.OUTPUT
+led2.direction = digitalio.Direction.OUTPUT     # LED setup
 
 button = digitalio.DigitalInOut(board.GP3)
 button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.DOWN
+button.pull = digitalio.Pull.DOWN               # button setup - pull down
 
-var = 10
+var = 10    # global var for end check
 
 while button.value == False:
-    print('ready for liftoff')
+    print('ready for liftoff')      # print until button pressed, then loop passed
 
 for x in range (10):
     print(10-x)
     var = x
     led1.value = True
     time.sleep(.5)
-    if button.value == True:
+    if button.value == True:    # somewhat did spicy version, code stops when button pressed
         break
     led1.value = False
     time.sleep(.5)
     if button.value == True:
         break
 
-if var == 0:
+if var == 0:            # only liftoff if code went uninterrupted
     print ("liftoff")
     led2.value = True
     time.sleep(5)
--- raspberry-pi/launchPad4Spicy.py
+++ raspberry-pi/launchPad4Spicy.py
@@ -2,36 +2,36 @@ import board
 import digitalio
 import time
 import pwmio
-from adafruit_motor import servo
+from adafruit_motor import servo    # motor lib
 
 led1 = digitalio.DigitalInOut(board.GP1)
 led1.direction = digitalio.Direction.OUTPUT
 led2 = digitalio.DigitalInOut(board.GP2)
-led2.direction = digitalio.Direction.OUTPUT
+led2.direction = digitalio.Direction.OUTPUT     # LED setup
 
 button = digitalio.DigitalInOut(board.GP3)
 button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.DOWN
+button.pull = digitalio.Pull.DOWN               # button setup - pull down
 
-pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)   # servo setup
 servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 servo1.angle = 0
 
 while button.value == False:
     print('ready for liftoff')
 
-for x in range (7):
+for x in range (7):     # countdown like normal for 7 seconds
     print(10-x)
     led1.value = True
     time.sleep(.5)
     led1.value = False
     time.sleep(.5)
 
-for x in range (3):
+for x in range (3):     # remaining 3 seconds
     print(3-x)
     led1.value = True
     for x in range (10):
-        servo1.angle = servo1.angle + 3
+        servo1.angle = servo1.angle + 3     # loops each total to .5 sec of delay
         time.sleep(.05)
     led1.value = False
     for x in range (10):
